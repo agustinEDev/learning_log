484bef3d94dae8653436bed11bff94190c4b0e39
agustinEDev <agustin_estevez_d@hotmail.com>
2024-03-23 12:28:59 +0100

    Página de inicio

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index (request):
+    """La página de inicio para learning_logs."""
+    return render(request, 'learning_logs/index.html')
