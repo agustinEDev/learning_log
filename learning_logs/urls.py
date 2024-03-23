e874a875431a92bb7e1778dbefdb638243c64a93
agustinEDev <agustin_estevez_d@hotmail.com>
2024-03-23 19:39:10 +0100

    Nuevas páginas

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Página de inicio
     path('', views.index, name = 'index'),
+    #Página que muestra todos los temas
+    path('topics', views.topics, name = 'topics'),
+    # Página de detalles para un tema individual
+    path('topics/<int:topic_id>/', views.topic, name = 'topic')
 ]
