53e1749ba1bac4d21e12ef07ccceba56dc4d4706
agustinEDev <agustin_estevez_d@hotmail.com>
2024-04-06 20:18:12 +0200

    Set DEBUG False on live Site

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1@!o+(^afl+ms9k-ar%_ji^kqce7b9=ya@h%eyg2jq4bk*f*zz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
